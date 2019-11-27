13556f458209cd0f17abb48b94648f70db5705ee
Gabriel Nori <gabrielnori@gabrielnori.tech>
2019-11-27 01:44:19 -0300

    It works... not as expected, but works

--- JS/aTUA.js
+++ JS/aTUA.js
@@ -1,4 +1,10 @@
+var images = [
+	"../Media/Images/Photos/01aTUA/Galeria/Galeria1.jpg",
+	"../Media/Images/Photos/01aTUA/Galeria/Galeria2.jpg",
+	"../Media/Images/Photos/01aTUA/Galeria/Galeria3.jpg"
+];
 function startFunction(){
-    setMenuBarClick();
+	setMenuBarClick();
+	setGallery(document.getElementById("bannerImg"), document.getElementById("bannerImg2"), images, 10);
 }
 document.addEventListener("DOMContentLoaded", startFunction);
--- JS/modules/gallery.js
+++ JS/modules/gallery.js
@@ -1,11 +1,37 @@
-var images = [];
+var 
+	imagesSrc,
+	actualImage,
+	nextImage,
+	interval,
+	totalImages,
+	maxIndex,
+	timed,
+	actual = 0;
 
-function changeImages(number){
-	if(!inRange(number, 0, images.length)){
-		number = 0;
+function setGallery(firstImage, secondImage, imagesIn, intervalIn){
+	imagesSrc = imagesIn;
+	actualImage = firstImage;
+	nextImage = secondImage;
+	interval = intervalIn*1000;
+	totalImages = images.length;
+	maxIndex = totalImages -1;
+	if(firstImage && secondImage){
+		timed = setInterval(changeImages, interval);
+	}
+	else{
+		console.log("something is terribly wrong with this shit");
 	}
 }
-
-function inRange(x, min, max) {
-	return ((x-min)*(x-max) <= 0);
+function changeImages(){
+	nextImage.className = "hidden";
+	let holder = actualImage;
+	actual += 1;
+	if(actual > maxIndex){
+		actual = 0
+	}
+	nextImage.src = images[actual];
+	actualImage.className = "slide-out";
+	nextImage.className = "slide-in";
+	actualImage = nextImage;
+	nextImage = holder;
 }

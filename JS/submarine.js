1cd608052e242a625afc971125486b4ec629abb2
Gabriel Nori <gabrielnori@gabrielnori.tech>
2019-11-27 02:32:03 -0300

    WORKING!! The css animation has bugs, but who cares when it works?

--- JS/submarine.js
+++ JS/submarine.js
@@ -1,4 +1,10 @@
+var images = [
+	"../Media/Images/Photos/07Submarine/Galeria/Galeria1.png",
+	"../Media/Images/Photos/07Submarine/Galeria/Galeria2.jpg",
+	"../Media/Images/Photos/07Submarine/Galeria/Galeria3.jpg",
+];
 function startFunction(){
-    setMenuBarClick();
+	setMenuBarClick();
+	setGallery(document.getElementById("bannerImg"), document.getElementById("bannerImg2"), images, 2);
 }
 document.addEventListener("DOMContentLoaded", startFunction);

d1166328bed289b5dbe15769e34889c0097061b6
Gabriel Nori <gabrielnori@gabrielnori.tech>
2019-11-27 18:23:20 -0300

    WORKS

--- JS/aTUA.js
+++ JS/aTUA.js
@@ -1,10 +1,9 @@
 var images = [
 	"../Media/Images/Photos/01aTUA/Galeria/Galeria1.jpg",
-	"../Media/Images/Photos/01aTUA/Galeria/Galeria2.jpg",
-	"../Media/Images/Photos/01aTUA/Galeria/Galeria3.jpg"
+	"../Media/Images/Photos/01aTUA/Galeria/Galeria2.jpg"
 ];
 function startFunction(){
 	setMenuBarClick();
-	setGallery(document.getElementById("bannerImg"), document.getElementById("bannerImg2"), images, 2);
+	setGallery(document.getElementById("bannerImg"), document.getElementById("bannerImg2"), images, 5);
 }
 document.addEventListener("DOMContentLoaded", startFunction);
